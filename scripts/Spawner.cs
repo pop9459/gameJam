a54912f49c410b47443817c8fd28712f81c4684f
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-23 07:36:03 +0100

    enemys now spawn periodiacly and have a random color

--- scripts/Spawner.cs
+++ scripts/Spawner.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Spawner : Node2D
 {
+	private double spawnDelay = 1.5f;
+	private double nextSpawn = 0f;
+	private double totalElapsed = 0;
 	[Export] private PackedScene enemy;
 	private Node2D player;
 	private string[] sprites = new string[] { "res://assets/monster.png", 
@@ -13,13 +16,17 @@ public partial class Spawner : Node2D
 	public override void _Ready()
 	{
 		player = GetParent().GetNode<Node2D>("Player");
-		spawnEnemy();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		totalElapsed += delta;
+		if(totalElapsed > nextSpawn)
+		{
+			nextSpawn = totalElapsed + spawnDelay;
+			spawnEnemy();
+		}		
 	}
 
 	private void spawnEnemy()
@@ -30,8 +37,14 @@ public partial class Spawner : Node2D
 		//set random sprite
 		int randomSpriteIndex = (int)GD.RandRange(0, sprites.Length-1);
 		newEnemy.GetNode<Sprite2D>("Sprite2D").Texture = GD.Load<Texture2D>(sprites[randomSpriteIndex]);
+		
 		//set random color
-		Color randomColor = new Color((float)GD.RandRange(0, 1), (float)GD.RandRange(0, 1), (float)GD.RandRange(0, 1));
+		int randomColorInt = GD.RandRange(1, 7);
+		//1 2 4
+		int r = (randomColorInt & 1) != 0 ? 1 : 0;
+		int g = (randomColorInt & 2) != 0 ? 1 : 0;
+		int b = (randomColorInt & 4) != 0 ? 1 : 0;
+		Color randomColor = new Color(r, g, b);
 		newEnemy.GetNode<Sprite2D>("Sprite2D").SelfModulate = randomColor;
 		AddChild(newEnemy);
 	}
