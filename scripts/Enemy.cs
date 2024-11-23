7439e63ba0891926bdf7ccbf904a28b5ca8dd5af
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-23 04:06:30 +0100

    added a spawner object and the enemy now moves towards the player

---
+++ scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Enemy : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	private Node2D player;
+	public void setPlayer(Node2D player)
+	{
+		this.player = player;
+	}
+    public override void _EnterTree()
+    {
+
+    }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+		if (player == null) return;
+		Vector2 direction = (player.GlobalPosition - GlobalPosition).Normalized();
+		GlobalPosition += direction * (float)delta * 100; // Adjust the speed as needed
+	}
+}
