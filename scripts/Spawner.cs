c6325e7308a6090559e5d55755660de6566fed02
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-23 06:58:59 +0100

    enemies now spawn with a random sprite design

--- scripts/Spawner.cs
+++ scripts/Spawner.cs
@@ -5,6 +5,10 @@ public partial class Spawner : Node2D
 {
 	[Export] private PackedScene enemy;
 	private Node2D player;
+	private string[] sprites = new string[] { "res://assets/monster.png", 
+											"res://assets/monsterWater.png", 
+											"res://assets/monsterBear.png", 
+											"res://assets/monsterFire.png" };
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -22,7 +26,11 @@ public partial class Spawner : Node2D
 	{
 		Enemy newEnemy = enemy.Instantiate<Enemy>();
 		newEnemy.setPlayer(player);
-
+		
+		//set random sprite
+		int randomSpriteIndex = (int)GD.RandRange(0, sprites.Length-1);
+		newEnemy.GetNode<Sprite2D>("Sprite2D").Texture = GD.Load<Texture2D>(sprites[randomSpriteIndex]);
+		//set random color
 		Color randomColor = new Color((float)GD.RandRange(0, 1), (float)GD.RandRange(0, 1), (float)GD.RandRange(0, 1));
 		newEnemy.GetNode<Sprite2D>("Sprite2D").SelfModulate = randomColor;
 		AddChild(newEnemy);
